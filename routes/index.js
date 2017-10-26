c41604db345518274db38cb06f8bfa3c08bcc7fe
Bill Xiong <wwx@duke.edu>
2017-10-25 22:08:11 -0400

    add callback

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.post('/pushNotification', function(req, res, next) {
         title : req.body.title,
         body: req.body.body,
         icon: req.body.icon
-    }), 
+    }),
     function(err, response) {
         res.json("finished");
     });
